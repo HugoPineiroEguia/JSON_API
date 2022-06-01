8374ea32ca08bc7c7054cd2fb557d5c700bf04b6
pinha <pinhaeguia@gmail.com>
2022-06-01 14:08:26 +0200

    Array comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -42,7 +42,8 @@ public class Main {
         JSONArray arr = obj.getJSONArray("posts");
         for (int i = 0; i < arr.length(); i++) {
             String post_id = arr.getJSONObject(i).getString("post_id");
-            System.out.println(post_id);
+            JSONArray comments = arr.getJSONObject(i).getJSONArray("comments");
+            System.out.println(comments);
         }
     }
 
